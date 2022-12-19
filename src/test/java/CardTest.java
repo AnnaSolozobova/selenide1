f57b601b8e91ca3056b7d7f5fb9864b1617b2994
AnnaSolozobova <solozobovaanna5@gmail.com>
2022-12-19 18:21:19 +0300

    modified Test

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,39 +1,32 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
-    @BeforeEach
-    void shouldOpenBrowser() {
-        open("http://localhost:9999/");
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
 
     @Test
-    public void happyPathTest() {
-        String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $x("//input[@placeholder=\"Дата встречи\"]").val(meetingDate);
-        SelenideElement form = $x("//form[contains(@class,form)]");
-        SelenideElement notif = $x("//div[@data-test-id=\"notification\"]");
-        $x("//input[@placeholder=\"Город\"]").val("Москва");
-        $x("//span[text()=\"Москва\"]").shouldHave(Condition.exactText("Москва"));
-        form.$x(".//span[@data-test-id=\"name\"]//input").val("Александр Иванов");
-        form.$x(".//span[@data-test-id=\"phone\"]//input").setValue("+79031243456");
-        form.$x(".//span[@class=\"checkbox__box\"]").click();
-        form.$x(".//span[text()=\"Забронировать\"]//ancestor::button").click();
-        notif.should(visible, ofSeconds(15));
-        notif.$x(".//div[@class='notification__title']").should(text("Успешно!"));
-        $("[data-test-id=notification] .notification__content").should(exactText("Встреча успешно забронирована на " + meetingDate));
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String currentDate = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79012345678");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
-
-
-
 }
